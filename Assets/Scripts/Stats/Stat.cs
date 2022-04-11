0a49055cf88a88b9103e41bb334b56dfc98352ce
João Rosário <joao.basto.rosario@gmail.com>
2022-04-11 12:28:51 +0100

    ADD: materials and code for stats; UPDATE: camera movement

---
+++ Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat
+{
+    [SerializeField]
+    public int baseValue;
+    
+
+    public int GetValue(){
+        return baseValue;
+    }
+}
