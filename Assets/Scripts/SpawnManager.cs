5ab9b0f5214ee2e21d1922285146d968d4c4bdc2
Diogo Santos <diogofilipe1_santos@hotmail.com>
2022-05-11 19:35:13 +0100

    Updated enemy spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,14 +9,15 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject enemy3;
 
     [SerializeField] private float range;
+    [SerializeField] private float awayDistanceFromPlayer;
     [SerializeField] private float coolDown;
 
-    Transform transform;
+    Transform player;
 
-    float nextSpawnTime = 0f;
+    float nextSpawnTime = 3f;
 
     void Start() {
-        transform = GetComponent<Transform>();
+        player = PlayerManager.instance.player.transform;
     }
 
     void Update()
@@ -31,17 +32,23 @@ public class SpawnManager : MonoBehaviour
     }
 
     void Spawn() {
-        GameObject enemyToSpawn = chooseEnemyToSpawn();
+        int numEnemiesToSpawn = Random.Range(1, 4);
+        int counter = 0;
 
-        float centerX = transform.position.x;
-        float centerZ = transform.position.z;
-
-        float x = Random.Range(centerX- range, centerX + range);
-        float z = Random.Range(centerZ- range, centerZ + range);
-
-        Instantiate(enemyToSpawn, new Vector3(x, 0, z), Quaternion.identity);
+        while (counter != numEnemiesToSpawn) {
+            SpawnEnemy();
+            
+            counter++;
+        }
 
         nextSpawnTime = Time.time + coolDown;
+    }
+
+    void SpawnEnemy() {
+        GameObject enemyToSpawn = chooseEnemyToSpawn();
+        Vector3 pointToSpawn = choosePointToSpawn();
+
+        Instantiate(enemyToSpawn, pointToSpawn, Quaternion.identity);
     }
 
     GameObject chooseEnemyToSpawn() {
@@ -50,5 +57,38 @@ public class SpawnManager : MonoBehaviour
         if (enemy == 0) return enemy1;
         else if(enemy == 1) return enemy2;
         else return enemy3;
+    }
+
+    Vector3 choosePointToSpawn() {
+        float playerX = player.position.x;
+        float playerZ = player.position.z;
+
+        float x = generateCoordinateAwayFromPlayerToSpawn(playerX);
+        float z = generateCoordinateAwayFromPlayerToSpawn(playerZ);
+        while (!validPosition(x, z)) {
+            x = generateCoordinateAwayFromPlayerToSpawn(playerX);
+            z = generateCoordinateAwayFromPlayerToSpawn(playerZ);
+        }
+
+        return new Vector3(x, 0, z);
+    }
+
+    bool validPosition(float x, float z) {
+        // verify if it's not already an enemy position
+        return true;
+    }
+
+    float generateCoordinateAwayFromPlayerToSpawn(float center) {
+        float coordinate = Random.Range(center - range, center + range);
+        
+        while (!awayFromPlayer(coordinate, center)) {
+            coordinate = Random.Range(center - range, center + range);
+        }
+
+        return coordinate;
+    }
+
+    bool awayFromPlayer(float point, float player) {
+        return Mathf.Abs(point - player) >= awayDistanceFromPlayer;
     }
 }
