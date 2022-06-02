0f2e43d2acb64fff1ebf80a907dab6fa2e4f5806
Diogo Santos <diogofilipe1_santos@hotmail.com>
2022-06-02 19:44:17 +0100

    player dies

--- Assets/Scripts/EndOfArenaCollider.cs
+++ Assets/Scripts/EndOfArenaCollider.cs
@@ -7,8 +7,14 @@ public class EndOfArenaCollider : MonoBehaviour
     float VERY_BIG_DAMAGE = 1000f;
 
     void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Third Person Player") {
+            PlayerModel[] playerModel = FindObjectsOfType<PlayerModel>();
+            if (playerModel.Length != 0) {
+                playerModel[0].TakeDamage(VERY_BIG_DAMAGE);
+            }
+        }
+
         CharacterModel model = other.gameObject.GetComponent<CharacterModel>();
-        Debug.Log(model);
         if (model != null) {
             model.TakeDamage(VERY_BIG_DAMAGE);
         }
